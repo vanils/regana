379a26f24b9d7e821a66d54c4e3b192d29640d24
vanils <matti@vanils.fi>
2018-05-15 21:10:56 +0300

    refactor: simplified node getter to only have one method

--- src/analyse/node.js
+++ src/analyse/node.js
@@ -1,28 +1,4 @@
 
-/*
- * Helper to find correct analyser
- */
-const getAnalyser = node => {
-  switch (node.type) {
-    case 'RegExpLiteral':
-      return require('./types/literals/regex');
-    case 'NullLiteral':
-      return require('./types/literals/null');
-    case 'StringLiteral':
-      return require('./types/literals/string');
-    case 'BooleanLiteral':
-      return require('./types/literals/boolean');
-    case 'NumericLiteral':
-      return require('./types/literals/numeric');
-    case 'VariableDeclaration':
-      return require('./types/declarations/variable');
-    case 'ExportNamedDeclaration':
-      return require('./types/modules/exports/exportNamed');
-    default:
-      throw new Error(`Failed to parse, unknown entity type '${node.type}'`);
-  }
-};
-
 /**
  * Analyse a single node. Can be considered as router for other node types.
  *
@@ -34,6 +10,25 @@ const getAnalyser = node => {
  * @example
  * analyseNode(node, scope);  // returns analyse
  */
-const analyseNode = (node, scope) => getAnalyser(node)(node, scope);
+const analyseNode = (node, scope) => {
+  switch (node.type) {
+    case 'RegExpLiteral':
+      return require('./types/literals/regex')(node, scope);
+    case 'NullLiteral':
+      return require('./types/literals/null')(node, scope);
+    case 'StringLiteral':
+      return require('./types/literals/string')(node, scope);
+    case 'BooleanLiteral':
+      return require('./types/literals/boolean')(node, scope);
+    case 'NumericLiteral':
+      return require('./types/literals/numeric')(node, scope);
+    case 'VariableDeclaration':
+      return require('./types/declarations/variable')(node, scope);
+    case 'ExportNamedDeclaration':
+      return require('./types/modules/exports/exportNamed')(node, scope);
+    default:
+      throw new Error(`Failed to parse, unknown entity type '${node.type}'`);
+  }
+};
 
 module.exports = analyseNode;
