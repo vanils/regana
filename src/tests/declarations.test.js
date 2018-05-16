b71d62660d4c74d7ef5de4a33349d04ad77bed98
vanils <matti@vanils.fi>
2018-05-16 20:38:37 +0300

    fix: support for debugger statement

--- src/tests/declarations.test.js
+++ src/tests/declarations.test.js
@@ -10,6 +10,7 @@ describe('regana', () => {
         expect(regana.analyseFile(path.resolve(__dirname, './mocks/declarations/variable/mock2.js'))).toMatchSnapshot();
         expect(regana.analyseFile(path.resolve(__dirname, './mocks/declarations/variable/mock3.js'))).toMatchSnapshot();
         expect(regana.analyseFile(path.resolve(__dirname, './mocks/declarations/variable/mock4.js'))).toMatchSnapshot();
+        expect(regana.analyseFile(path.resolve(__dirname, './mocks/declarations/variable/mock5.js'))).toMatchSnapshot();
       });
     });
   });
